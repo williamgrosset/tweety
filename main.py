7550accd24f5209d3697c1892344c71755099825
William Grosset <williamhgrosset@gmail.com>
2018-01-10 22:46:15 -0800

    Collect entire response

--- main.py
+++ main.py
@@ -2,9 +2,18 @@
 import socket
 import ssl
 
+def recv_timeout(socket):
+    total_data = []
+    while True:
+        data = socket.recv(4096)
+        if not data: break
+        total_data.append(data)
+    return b''.join(total_data)
+
 # TODO: parse URL from command-line arg(s)
 host_ip = socket.gethostbyname('www.ubc.ca')
 
+# TODO: Handle regular http (w/o SSL wrap)
 # Initialize socket
 # Sockt connection on port 80
 # Hit http request (if we hit a 301 or 302 - target https support boolean flag)
@@ -16,17 +25,14 @@ uw = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s = ssl.wrap_socket(uw, ssl_version=ssl.PROTOCOL_TLS)
 s.connect((host_ip, 443))
 
-# TODO: Handle regular http (w/o SSL wrap)
-
 # Connect socket and send request
-s.send('GET / HTTP/1.0\r\n\r\n'.encode('utf-8'))
+s.send('GET http://www.ubc.ca HTTP/1.0\r\n\r\n'.encode('utf-8'))
 
 # TODO:
 #   Header:
 #   + getuseragent() from a python lib?
 
-# Receive response on socket
-resp = s.recv(4096)
+resp = recv_timeout(s)
 print(resp)
 
 s.close()
