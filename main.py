939b0621f7fc626a37ba70abcdbdcd0b99d59460
William Grosset <williamhgrosset@gmail.com>
2018-01-10 18:51:41 -0800

    SSL wrap

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python
 
 import socket
+import ssl
 
 # TODO: parse URL from command-line arg(s)
+host_ip = socket.gethostbyname('www.facebook.com')
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Initialize socket, wrap in SSL, and connect to host ip
+uw = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = ssl.wrap_socket(uw, ssl_version=ssl.PROTOCOL_TLS)
+s.connect((host_ip, 443))
 
-# TODO: error handling support for port
-host_ip = socket.gethostbyname('www.github.com')
-s.connect((host_ip, 80))
+# Connect socket and send request
+s.send('GET https://www.facebook.com/unsupportedbrowser HTTP/1.0\r\n\r\n'.encode('utf-8'))
 
 # TODO: getuseragent() from a python lib?
-s.send('GET https://www.github.com/ HTTP/1.0\r\n\r\n'.encode('utf-8'))
-
 # TODO: If 302 and http(s), wrap socket in a SSL
 
-
-
+# Receive response on socket
 resp = s.recv(4096)
 print(resp)
 
