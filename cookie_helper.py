867acbccdc6019d6fb5b43dbaa109da537974ef0
William Grosset <williamhgrosset@gmail.com>
2018-01-19 15:56:36 -0800

    Retrieve all cookie entries into array

--- cookie_helper.py
+++ cookie_helper.py
@@ -1,6 +1,6 @@
 import re
 
 def get_cookies(response):
-    cookie_match = re.search('Set-Cookie: (.*)', response)
-    if cookie_match: print(cookie_match.group())
+    cookie_match = re.findall('Set-Cookie: (.*)', response)
+    if cookie_match: print(cookie_match), print(len(cookie_match))
     else: print('None.')
