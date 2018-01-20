fd2d6ec5717e2a2375b3ee6408a5736a8ac2e367
William Grosset <williamhgrosset@gmail.com>
2018-01-19 18:28:32 -0800

    Print statements for results

--- results_helper.py
+++ results_helper.py
@@ -1,17 +1,17 @@
 def print_website(url):
-    cookie_match = re.findall('Set-Cookie: (.*)', response)
-    if cookie_match: print(cookie_match), print(len(cookie_match))
-    else: print('None.')
+    print('website: ' + url)
 
-# def print_https_support(location):
+def print_https_support(supports_http):
+    print('1. Support of HTTPS: ' + supports_http)
 
-# def print_newest_http_version(http):
+def print_newest_http_version(http_version):
+    print('2. The newest HTTP versions that the web server supports: ' + http_version)
 
-# def print_cookies(cookies_collection):
+def print_cookies(cookies):
+    print('3. List of cookies:')
 
-'''
 def print_results():
-    print_website()
+    print_website(url)
     print_https_support(location)
-    print_cookies()
-'''
+    print_newest_http_version(http_version)
+    print_cookies(cookies)
