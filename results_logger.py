a05d12058c9cf0418844b4a39d1e7d6ae8818f77
William Grosset <williamhgrosset@gmail.com>
2018-01-21 16:40:45 -0800

    Fix cookie key regex

--- results_logger.py
+++ results_logger.py
@@ -16,11 +16,11 @@ def print_cookies(cookies):
     for cookie in cookies:
         # TODO: Stricter regex
         name_match = re.match('name=(.*)', cookie)
-        key_match = re.search('^\w*', cookie)
+        key_match = re.search('([\w\.-]*)=', cookie)
         domain_name_match = re.match('.*domain=([\w\.-]*)', cookie)
 
         if name_match: name = name_match.group(1)
-        if key_match: key = key_match.group(0)
+        if key_match: key = key_match.group(1)
         if domain_name_match: domain_name = domain_name_match.group(1)
 
         print('name: ' + name + ', key: ' + key + ', domain name: ' + domain_name)
